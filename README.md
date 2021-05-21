387c4a083bc875750158328b011c67f9815d4ae0
Virdi <vrd_gursharn@hotmail.com>
2021-05-21 16:29:19 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- SASS 
+- Mobile-first workflow
+
+### What I learned
+
+Improved my knowlegde with APIs
+
+## Author
+
+- Website - [Gursharn Singh Virdi]
+- Frontend Mentor - [@gsv93](https://www.frontendmentor.io/profile/gsv93)
---
+++ index.js
@@ -0,0 +1,60 @@
+//Getting HTML elements
+const getIP = document.getElementById("ipAdressField");
+const getLoaction = document.getElementById("locationField");
+const getTimeZone = document.getElementById("timeZoneField");
+const getISP = document.getElementById("ispField");
+
+//API key
+const geo_api_key = 'at_Bv1Nt8sx0rKdFwooe9snR5R3NZQ3v';
+
+
+let latitude;
+let longitude;
+
+/*Get User Ipadress
+fetch("https://api.ipify.org?format=json")
+    .then(results => results.json())
+    .then(data => {
+           console.log(data.ip);
+           
+}) */
+
+var mymap = L.map('mapid').setView([50.1109, 8.6821], 13);
+const attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+let tiles = L.tileLayer(tileUrl, {attribution});
+tiles.addTo(mymap);
+
+function updateMap(latitude, longitude) {
+    //Updates Map
+    mymap.panTo([latitude, longitude]);
+    //Location Marker
+    var marker = L.marker([latitude, longitude]).addTo(mymap);
+    marker.bindPopup("Your Location").openPopup();
+}
+
+
+function searchLocation() {
+    var getIpadress = $("#inputField").val();
+    let requestUrl = "https://geo.ipify.org/api/v1?apiKey=" + geo_api_key + "&ipAddress=" + getIpadress;
+    fetch(requestUrl)
+        .then(results => results.json())
+        .then((data) => {
+            console.log(data);
+            getIP.innerHTML = data.ip;
+            getLoaction.innerHTML = data.location.city;
+            getTimeZone.innerHTML = "UTC " + data.location.timezone;
+            getISP.innerHTML = data.isp;
+
+            latitude = data.location.lat;
+            longitude = data.location.lng;
+
+            updateMap(latitude, longitude);
+            
+        })
+        .catch(error => {
+            console.log(error);
+            alert("Input is not an valid IP")
+        })
+}
+
